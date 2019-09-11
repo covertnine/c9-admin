d57f47978a26d3a460a57b291655a020a79d7592
samirillion <sam@protonmail.com>
2019-09-11 10:29:56 -0500

    working through sniffer

--- c9-admin.php
+++ c9-admin.php
@@ -1,12 +1,10 @@
 <?php
-
 /**
  * C9 Admin Base
  *
- *
- * @link              https://www.covertnine.com
- * @since             1.0.0
- * @package           C9_Admin
+ * @package C9_Admin
+ * @link    https://www.covertnine.com
+ * @since   1.0.0
  *
  * @wordpress-plugin
  * Plugin Name:       C9 Admin
@@ -17,21 +15,21 @@
  * Author URI:        https://www.covertnine.com
  * License:           GPL-2.0+
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       c9-admin
+ * Text Domain:   ∏    c9-admin
  * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
-if (!defined('WPINC')) {
+if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-if (!defined('ATTACHMENT_REDIRECT_CODE')) {
-	define('ATTACHMENT_REDIRECT_CODE', '301'); // Default redirect code for attachments with existing parent post
+if ( ! defined( 'ATTACHMENT_REDIRECT_CODE' ) ) {
+	define( 'ATTACHMENT_REDIRECT_CODE', '301' ); // Default redirect code for attachments with existing parent post.
 }
 
-if (!defined('ORPHAN_ATTACHMENT_REDIRECT_CODE')) {
-	define('ORPHAN_ATTACHMENT_REDIRECT_CODE', '302'); // Default redirect code for attachments with no parent post
+if ( ! defined( 'ORPHAN_ATTACHMENT_REDIRECT_CODE' ) ) {
+	define( 'ORPHAN_ATTACHMENT_REDIRECT_CODE', '302' ); // Default redirect code for attachments with no parent post.
 }
 
 /**
@@ -39,14 +37,14 @@ if (!defined('ORPHAN_ATTACHMENT_REDIRECT_CODE')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('C9_ADMIN_VERSION', '1.0.0');
+define( 'C9_ADMIN_VERSION', '1.0.0' );
 
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path(__FILE__) . 'admin/class-c9-admin.php';
+require plugin_dir_path( __FILE__ ) . 'admin/class-c9-admin.php';
 
 /**
  * Begins execution of the plugin.
@@ -55,11 +53,9 @@ require plugin_dir_path(__FILE__) . 'admin/class-c9-admin.php';
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.0.0
+ * @since 1.0.0
  */
-function run_c9_admin()
-{
-
-	$plugin = new C9_Admin('C9_Admin', C9_ADMIN_VERSION);
+function run_c9_admin() {
+   $plugin = new C9_Admin( 'C9_Admin', C9_ADMIN_VERSION );
 }
 run_c9_admin();
